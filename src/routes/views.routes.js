440a23075b82a1e61179ffd4e0ee2d5dd4b665d1
Christian Alcalde <alcalde_christian@hotmail.com>
2024-12-22 15:47:16 -0300

    handlebars /products OK

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import ProductManager from "../services/fs/ProductManager.js";
+import ProductManager from "../services/db/ProductManager.js";
 
 
 const router = Router()
@@ -7,13 +7,42 @@ const productManager = new ProductManager()
 
 
 // Rutas de Handlebars
-router.get("/", async (req, res) => {
+router.get("/products", async (req, res) => {
     try {
-        const products = await productManager.getAll()
-        res.render("index", {products})
+        const limit = req.query.limit ? parseInt(req.query.limit) : 10
+        const page = req.query.page ? parseInt(req.query.page) : 1
+        const filter = req.query.filter ? { category: req.query.filter } : {}
+        const sort = req.query.sort ? { price: req.query.sort === "asc" ? 1 : -1 } : undefined
+
+        const products = await productManager.getAll(limit, page, filter, sort)
+
+        console.log(products)
+        
+        if (limit <= 0 || page <= 0) {
+            return res.status(400).json({success: false, error: "Los valores de limit y page deben ser mayores que 0"})
+        }
+
+        if (products.docs.length === 0) {
+            return res.status(404).json({success: false, error: "No se encontraron productos que cumplan con el filtro solicitado"})
+        }
+
+        products.prevLink = products.hasPrevPage ? `http://localhost:8080/products?limit=${limit}&page=${products.prevPage}&filter=${req.query.filter || ``}&sort=${req.query.sort || ''}` : ''
+
+        products.nextLink = products.hasNextPage ? `http://localhost:8080/products?limit=${limit}&page=${products.nextPage}&filter=${req.query.filter || ``}&sort=${req.query.sort || ''}` : ''
+
+        products.isValid = !(page <= 0 || page > products.totalPages)
+
+        res.render("index", products)
     } catch (error) {
         console.log(error)
     }
+
+
+
+
+
+
+
 })
 
 /*
